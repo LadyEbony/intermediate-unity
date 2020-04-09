f79b0f05d6a5cb7ccac8c845247cd8a16d4db376
LadyEbony <jgarc129@ucr.edu>
2020-04-08 20:55:18 -0700

    pushing random files

---
+++ Assets/Code/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour {
+
+  public NavMeshAgent nva;
+  public float speed;
+
+  // Update is called once per frame
+  void Update() {
+    var hor = Input.GetAxisRaw("Horizontal");
+    var ver = Input.GetAxisRaw("Vertical");
+    var steering = new Vector3(hor, 0f, ver);
+    var delta = steering * speed * Time.deltaTime;
+    nva.Move(delta);
+  }
+}
