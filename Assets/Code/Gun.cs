51f0446b7ac28bffefa76790a855ea6f74391a3f
maxt0214 <49177628+maxt0214@users.noreply.github.com>
2020-04-15 15:11:25 -0700

    Shooting
    Added shooting bullets

--- Assets/Code/Gun.cs
+++ Assets/Code/Gun.cs
@@ -3,20 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Gun : MonoBehaviour {
+    public GameObject bulletPrefab;
+    public Transform firePoint;
 
-  // Start is called before the first frame update
-  void Start() {
-        
-  }
+    public float bulletSpeed = 10f;
+    public float fireRate = 1f;
+    public float bulletDestroyTime = 5f;
+
+    private float timeToFire = 0f;
+
+    // Start is called before the first frame update
+    void Start() {
+        if(bulletPrefab == null)
+        {
+            Debug.LogError("Gun: No bullet is attached!");
+        }
+        if(firePoint == null)
+        {
+            Debug.LogError("Gun: No Fire Point is found");
+        }
+    }
 
   // Update is called once per frame
-  void Update() {
-    HandleMouse();
-  }
+    void Update() {
+        HandleMouse();
+    }
 
-  void HandleMouse(){
-    var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-    transform.rotation = Quaternion.LookRotation(ray.direction, Vector3.up);
-  }
+    void HandleMouse(){
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        transform.rotation = Quaternion.LookRotation(ray.direction, Vector3.up);
 
+        if (Input.GetMouseButton(0) && Time.time >= timeToFire)
+        {
+            Shoot(ray.direction);
+            timeToFire = Time.time + 1 / fireRate;
+        }
+    }
+
+    void Shoot(Vector3 direction)
+    {
+        Transform bulletClone = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation).transform;
+        bulletClone.Translate(bulletSpeed * direction * Time.deltaTime);
+
+        Destroy(bulletClone.gameObject, bulletDestroyTime);
+    }
 }
---
+++ Assets/Code/MoveBullet.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBullet : MonoBehaviour
+{
+    public Transform Gun;
+
+    float moveSpeed;
+
+    private void Awake()
+    {
+        Gun = GameObject.Find("Gun").transform;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Gun == null)
+        {
+            Debug.LogError("MoveBullet: No gun is found");
+        }
+        if(Gun.GetComponent<Gun>() != null)
+        {
+            moveSpeed = Gun.GetComponent<Gun>().bulletSpeed;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //deal with bullet movement
+        Move();
+        //deal with bullet effect
+        Effect();
+    }
+
+    void Move()
+    {
+        transform.Translate(moveSpeed * Vector3.forward * Time.deltaTime);
+    }
+
+    void Effect()
+    {
+
+    }
+}
