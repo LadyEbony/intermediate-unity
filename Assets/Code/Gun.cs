3753afaf8e18b0162324a0e216eb81a5d7c1f270
LadyEbony <jgarc129@ucr.edu>
2020-04-15 12:34:42 -0700

    Staging for Gun Implementation

---
+++ Assets/Code/Gun.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+  // Start is called before the first frame update
+  void Start() {
+        
+  }
+
+  // Update is called once per frame
+  void Update() {
+    HandleMouse();
+  }
+
+  void HandleMouse(){
+    var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    transform.rotation = Quaternion.LookRotation(ray.direction, Vector3.up);
+  }
+
+}
