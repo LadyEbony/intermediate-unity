4c0fba29495e2e980bc61b54e2f233330ce0bc19
Rick Lee - rlee037 <rlee037@ucr.edu>
2020-04-15 14:37:42 -0700

    Ability Functionality Implemented
    Added abstract class "Ability" as a base for all abilities
    
    Added class "AbilityGrenade" that spawns a grenade that moves towards the direction the mouse is pointing
    
    Added class "Explosion" to allow the grenade to damage the environment

---
+++ Assets/Code/Abilities/Grenade/AbilityGrenade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityGrenade : Ability
+{
+    public GameObject grenade;
+    public float speed;
+    
+    public override void Activate() {
+        //Debug.Log("Grenade abiltity activated");
+        GameObject newGrenade = Instantiate(grenade, transform.position, transform.rotation);
+        newGrenade.SetActive(true);
+        
+        //Make the new object move in direction the mouse is pointing
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        newGrenade.GetComponent<Rigidbody>().velocity = new Vector3(ray.direction.x * speed, ray.direction.y * speed, ray.direction.z * speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) { //left mouse button
+            //Debug.Log("Button press detected");
+            Activate();
+        }
+    }
+}
---
+++ Assets/Code/Abilities/Grenade/Explosion.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public  float     detonationTime;
+    public  float     explosionRadius;
+    public  LayerMask explosionLayer;
+    
+    public  GameObject debris; //needs layer value "Debris" and the debris object must have a rigidbody and collider
+    
+    private float timeElapsed;
+    
+    public void Explode() {
+        Debug.Log("ドッカーン");
+        
+        //spawn debris
+        for (int i = 0; i < 10; ++i) {
+            //Debug.Log("generated debris");
+            GameObject newDebris = Instantiate(debris, transform.position, transform.rotation);
+            newDebris.SetActive(true);
+        }
+        
+        //destroy everything in the radius and destroy this object
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, explosionRadius, explosionLayer);
+        for (int i = 0; i < hitColliders.Length; ++i) {
+            Destroy(hitColliders[i].gameObject);
+        }
+        Destroy(gameObject);
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeElapsed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed >= detonationTime) {
+            Explode();
+        }
+    }
+}
